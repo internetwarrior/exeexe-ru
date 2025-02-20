800d5ed3ef080c43a89c9fe2e4641ef64aff04e1
Kairat Bektov <64087609+internetwarrior@users.noreply.github.com>
2025-02-20 12:16:48 +0600

    fix

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
@@ -10,9 +10,9 @@ def index(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user.urls')),
-    path('', index, name='index'),
     path('api/', include('chat.urls')),
     path('api/', include('post.urls')),
+    re_path(r'^.*$', index, name='index'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
